9518d6133249328d1dbe94d55f96e9a3005402e9
GoblinCwl <qwaszx123>
2020-05-25 20:33:16 +0800

    配置HTTP强转 优化一些内容

--- src/main/java/com/goblincwl/dragontwilight/DragontwilightApplication.java
+++ src/main/java/com/goblincwl/dragontwilight/DragontwilightApplication.java
@@ -1,13 +1,54 @@
 package com.goblincwl.dragontwilight;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DragontwilightApplication {
+
+    @Value("${server.port}")
+    private Integer httpsPort;
+
+    @Value("${server.http.port}")
+    private Integer httpPort;
 
     public static void main(String[] args) {
         SpringApplication.run(DragontwilightApplication.class, args);
     }
 
+    @Bean
+    public TomcatServletWebServerFactory servletContainer() {
+        //		TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() { ... }
+
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+        tomcat.addAdditionalTomcatConnectors(connector());
+        return tomcat;
+    }
+
+    @Bean
+    public Connector connector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        connector.setPort(httpPort);
+        connector.setSecure(false);
+        connector.setRedirectPort(httpsPort);
+        return connector;
+    }
 }
