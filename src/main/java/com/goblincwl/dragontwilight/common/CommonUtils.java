3a8c912ef957d0f1018548e705028026dd8ebeb8
GoblinCwl <qwaszx123>
2020-05-27 09:10:25 +0800

    优化后台管理

--- src/main/java/com/goblincwl/dragontwilight/common/CommonUtils.java
+++ src/main/java/com/goblincwl/dragontwilight/common/CommonUtils.java
@@ -1,11 +1,15 @@
 package com.goblincwl.dragontwilight.common;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import org.apache.http.HttpEntity;
 import org.apache.http.ParseException;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
@@ -16,7 +20,9 @@ import org.springframework.stereotype.Component;
 import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.net.InetAddress;
+import java.net.URLEncoder;
 import java.net.UnknownHostException;
 
 public class CommonUtils {
@@ -94,6 +100,104 @@ public class CommonUtils {
         return resultJson;
     }
 
+
+    /**
+     * POST无参
+     *
+     * @param url
+     * @param params
+     * @return void
+     * @create 2020/5/27 0:39
+     * @author ☪wl
+     * @document
+     */
+    public JSONObject doPostTestFour(String url, String params) {
+        JSONObject resultJson = null;
+        // 获得Http客户端(可以理解为:你得先有一个浏览器;注意:实际上HttpClient与浏览器是不一样的)
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+        // 创建Post请求
+        HttpPost httpPost = new HttpPost(url + "?" + params);
+        // 设置ContentType(注:如果只是传普通参数的话,ContentType不一定非要用application/json)
+        httpPost.setHeader("Content-Type", "application/json;charset=utf8");
+
+        // 响应模型
+        CloseableHttpResponse response = null;
+        try {
+            // 由客户端执行(发送)Post请求
+            response = httpClient.execute(httpPost);
+            // 从响应模型中获取响应实体
+            HttpEntity responseEntity = response.getEntity();
+
+            if (responseEntity != null) {
+                resultJson = JSONObject.parseObject(EntityUtils.toString(responseEntity));
+            }
+        } catch (ParseException | IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                // 释放资源
+                if (httpClient != null) {
+                    httpClient.close();
+                }
+                if (response != null) {
+                    response.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return resultJson;
+    }
+
+    public JSONObject doPostTestTwo(String url, Object object) {
+        JSONObject resultJson = null;
+        // 获得Http客户端(可以理解为:你得先有一个浏览器;注意:实际上HttpClient与浏览器是不一样的)
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+        // 创建Post请求
+        HttpPost httpPost = new HttpPost(url);
+        // 我这里利用阿里的fastjson，将Object转换为json字符串;
+        // (需要导入com.alibaba.fastjson.JSON包)
+        String jsonString = JSON.toJSONString(object);
+        StringEntity entity = new StringEntity(jsonString, "UTF-8");
+        // post请求是将参数放在请求体里面传过去的;这里将entity放入post请求体中
+        httpPost.setEntity(entity);
+        httpPost.setHeader("Content-Type", "application/json;charset=utf8");
+        // 响应模型
+        CloseableHttpResponse response = null;
+        try {
+            // 由客户端执行(发送)Post请求
+            response = httpClient.execute(httpPost);
+            // 从响应模型中获取响应实体
+            HttpEntity responseEntity = response.getEntity();
+
+            if (responseEntity != null) {
+                resultJson = JSONObject.parseObject(EntityUtils.toString(responseEntity));
+            }
+        } catch (ParseException | IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                // 释放资源
+                if (httpClient != null) {
+                    httpClient.close();
+                }
+                if (response != null) {
+                    response.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return resultJson;
+    }
+
+    /**
+     * 获取服务器地址
+     *
+     * @return java.lang.String
+     * @create 2020/5/27 0:35
+     * @author ☪wl
+     */
     public static String getServerUrl(HttpServletRequest request) {
         return "https://" + request.getServerName() + ":" + request.getServerPort() + request.getContextPath();
     }
